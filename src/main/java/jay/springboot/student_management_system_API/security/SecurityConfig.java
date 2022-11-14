99266b9790a0c2f51fba161041084d95f9ef3bc4
JayaminiAb <jayamini.amj@gmail.com>
2022-11-14 23:41:22 +0530

    Adding security configs

---
+++ src/main/java/jay/springboot/student_management_system_API/security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package jay.springboot.student_management_system_API.security;
+
+public class SecurityConfig {
+    //test
+}
--- src/main/java/jay/springboot/student_management_system_API/sequrity/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package jay.springboot.student_management_system_API.sequrity;
-
-public class SecurityConfig {
-    //test
-}
